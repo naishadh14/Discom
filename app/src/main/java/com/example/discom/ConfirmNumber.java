7887d94523097e4012edba23562dcfb110b8699d
Naishadh <voranaishadh@gmail.com>
2020-05-02 06:08:00 +0530

    Added updated UI for confirm number screen

--- app/src/main/java/com/example/discom/ConfirmNumber.java
+++ app/src/main/java/com/example/discom/ConfirmNumber.java
@@ -7,12 +7,13 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ConfirmNumber extends AppCompatActivity {
 
@@ -23,70 +24,69 @@ public class ConfirmNumber extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.confirm_number);
+
+        //try to get mobile number of user, if previously saved
         this.sharedPref = this.getSharedPreferences("MY_SETTINGS", Context.MODE_PRIVATE);
-        if (this.sharedPref.contains("PhoneNumber")) {
-            EditText text = findViewById(R.id.editText);
+        if(this.sharedPref.contains("PhoneNumber")) {
+            //sets value of textfield in case mobile number is found
+            TextInputEditText text = findViewById(R.id.editText);
             text.setText(Long.toString(this.sharedPref.getLong("PhoneNumber", 0)));
         }
+
+        //check if location permission enabled
+        //if not, ask for permission
         checkPermissions();
     }
 
     public void confirm(View view) {
 
-        //make sure app has location permissions, else return
+        //make sure app has location permissions, else do not proceed
         if(!checkPermissions()) {
             Toast toast = Toast.makeText(getApplicationContext(), "Please grant location access to continue", Toast.LENGTH_SHORT);
             toast.show();
             return;
         }
 
-        EditText text = findViewById(R.id.editText);
+        //get handle to textfield
+        TextInputEditText text = findViewById(R.id.editText);
         String number_text = text.getText().toString();
+
+        //show toast message if mobile number entered is not 10 digits
         if (number_text.length() != 10) {
             Toast toast = Toast.makeText(getApplicationContext(), "Phone Number must be 10 digits", Toast.LENGTH_SHORT);
             toast.show();
             return;
         }
+
+        //save mobile number to SharedPreferences for future reference
         long number = Long.parseLong(number_text);
         SharedPreferences.Editor editor = this.sharedPref.edit();
         editor.putLong("PhoneNumber", number);
         editor.apply();
+
+        //open next screen
         Intent intent = new Intent(this, DeviceDiscovery.class);
         startActivity(intent);
     }
 
+    public void cancel(View view) {
+        TextInputEditText text = findViewById(R.id.editText);
+        text.setText("");
+    }
+
     boolean checkPermissions() {
+        //checks if permission already granted
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
+            //permission not granted, ask user to allow
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_LOCATION);
             return false;
         }
         else
+            //already granted
             return true;
     }
-
-    /*
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission
-                }
-                return;
-            }
-
-        }
-    }
-     */
 }
